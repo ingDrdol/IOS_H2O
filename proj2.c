693d2709570ae0b6948fb641952da2082fec9071
ingDrdol <xkocma09@stud.fit.vutbr.cz>
2022-04-26 13:03:16 +0200

    zpracovani argumentu

--- proj2.c
+++ proj2.c
@@ -6,8 +6,42 @@
 
 int main(int argc, char **argv)
 {
-    int No, Nh, 
+    int no, nh, ti, tb;
+    char *rest;
+
     if(argc != 5)
+    {
+        fprintf(stderr, "Nespravny pocet argumentu\n"):
         return -1;
+    }
+    
+    no = strtol(argv[1], rest, 10);
+    if(*rest != '\0')
+    {
+        fprintf(stderr, "Pocet NO nevalidni\n");
+        return -1;
+    }
+
+    nh = strtol(argv[1], rest, 10);
+    if(*rest != '\0')
+    {
+        fprintf(stderr, "Pocet NH nevalidni\n");
+        return -1;
+    }
+
+    ti = strtol(argv[1], rest, 10);
+    if(*rest != '\0' || ti < 0 || ti > 100)
+    {
+        fprintf(stderr, "TI nevalidni\n");
+        return -1;
+    }
+
+    tb = strtol(argv[1], rest, 10);
+    if(*rest != '\0' || ti < 0 || ti > 100)
+    {
+        fprintf(stderr, "TB nevalidni\n");
+        return -1;
+    }
+    
     return 0;
 }
