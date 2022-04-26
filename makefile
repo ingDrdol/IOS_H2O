10baa42ab2111f156af1c1e5645bf85bea3efe15
Koci <xkocma09@stud.fit.vutbr.cz>
2022-04-26 17:12:51 +0200

    merge 1

--- makefile
+++ makefile
@@ -1,9 +1,9 @@
 CC = gcc
-CFLAGS = -std=c99 -Wall -Wextra -pedantic -g
+CFLAGS = -std=c99 -Wall -Wextra -pedantic -Werror -g
 
 all: proj2
 
-proj2: proj2.c
+proj2: proj2.c makefile
 	$(CC) $(CFLAGS) proj2.c -o proj2
 
 clear: 
--- proj2.c
+++ proj2.c
@@ -6,8 +6,39 @@
 
 int main(int argc, char **argv)
 {
-    int No, Nh, 
+    long no, nh, ti, tb;
+    char *rest = NULL;
     if(argc != 5)
+    {
+        fprintf(stderr, "nevalidni pocet argumentu\n");
         return -1;
+    }
+    
+    no = strtol(argv[1], &rest, 10);
+    if(*rest != '\0')
+    {
+        fprintf(stderr, "argument NO neni validni\n");
+    }
+    
+    nh = strtol(argv[2], &rest, 10);
+    if(*rest != '\0')
+    {
+        fprintf(stderr, "argument NH neni validni\n");
+    }
+    
+    ti = strtol(argv[3], &rest, 10);
+    if(*rest != '\0' || ti < 0 || ti > 1000)
+    {
+        fprintf(stderr, "argument TI neni validni\n");
+    }
+    
+    tb = strtol(argv[4], &rest, 10);
+    if(*rest != '\0' || tb < 0 || tb > 1000)
+    {
+        fprintf(stderr, "argument TB neni validni\n");
+    }
+    
+    printf("%ld, %ld, %ld, %ld\n", no, nh, ti, tb);
+    
     return 0;
 }
